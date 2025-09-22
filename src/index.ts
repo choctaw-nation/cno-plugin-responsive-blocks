9e7da4aa9ff708d3b4bb462a95f006777a2e2310
KJ Roelke <kroelke@choctawnation.com>
2025-09-22 10:56:07 -0500

    refactor plugin js

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,47 @@
+// WP Deps
 import domReady from '@wordpress/dom-ready';
-import addResponsiveControls from './addResponsiveControls';
+import { addFilter } from '@wordpress/hooks';
+
+// Callbacks
+import registerAttributes from './registerAttributes';
+import registerControls from './registerControls';
+import addAttributesToSave from './addAttributesToSave';
+import addAttributesToEditor from './addAttributesToEditor';
+
+// Styles
 import './editor.scss';
 import './style.scss';
 
-function alterBlocks(): void {
+domReady( () => {
 	const namespace = 'cno';
-	addResponsiveControls( namespace );
-}
-domReady( alterBlocks );
+	const hooks = [
+		{
+			hook: 'blocks.registerBlockType',
+			namespace: `${ namespace }/register-attributes`,
+			callback: registerAttributes,
+		},
+		{
+			hook: 'editor.BlockEdit',
+			namespace: `${ namespace }/register-controls`,
+			callback: registerControls,
+		},
+		{
+			hook: 'blocks.getSaveContent.extraProps',
+			namespace: `${ namespace }/add-attributes-to-save`,
+			callback: addAttributesToSave,
+		},
+		{
+			hook: 'editor.BlockListBlock',
+			namespace: `${ namespace }/add-attributes-to-editor`,
+			callback: addAttributesToEditor,
+		},
+	];
+	try {
+		hooks.forEach( ( { hook, namespace, callback } ) => {
+			addFilter( hook, namespace, callback );
+		} );
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.error( `Error initializing Responsive Controls:`, error );
+	}
+} );
